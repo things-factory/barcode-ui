a06c6043eff01b61fd00ebe85ff9f34293e29c8a
Murky <moosung.gil@hatiolab.com>
2019-09-02 14:22:32 +0900

    optional barcode types

--- things-factory-native.config.js
+++ things-factory-native.config.js
@@ -1,15 +1,21 @@
 module.exports = {
-  onMessage: function({ iab, type }) {
+  onMessage: function({ iab, type, detail = {} }) {
     if (type != 'scan-barcode') return
+    var { barcodeTypes = [], requester } = detail
+
+    var barcodes = barcodeTypes.join(',') || null
+
     cordova.plugins.barcodeScanner.scan(
       function(result) {
         iab.executeScript({
           code: `window.dispatchEvent(
             new CustomEvent('barcode-respond', {
-              detail: "${result.text}"
+              detail: {
+                value: "${result.text}",
+                responder: "${requester}"
+              }
             })
-          );
-          window.location = "${result.text}";`
+          );`
         })
       },
       function(error) {
@@ -22,6 +28,7 @@ module.exports = {
         saveHistory: true, // Android, save scan history (default false)
         prompt: 'Place a barcode inside the scan area', // Android
         resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats: barcodes,
         // formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
         disableAnimations: false, // iOS
         disableSuccessBeep: false // iOS and Android
